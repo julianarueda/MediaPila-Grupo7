b292f9467012a69a05ed5371187a4758d8ae8cd8
Juli <julianarueda40@gmail.com>
2024-07-17 14:35:22 -0300

    agregamos eventos

--- javascript/eventos.js
+++ javascript/eventos.js
@@ -36,7 +36,6 @@ class Domingueando extends Evento{
 
 function mostrarEventos(eventosAMostrar) {
     const eventosContainer = document.getElementById('eventos-container');
-
     // Limpiar el contenedor antes de agregar los eventos
     eventosContainer.innerHTML = '';
 
@@ -60,6 +59,7 @@ function mostrarEventos(eventosAMostrar) {
     // Agregar el evento al contenedor principal
         eventosContainer.appendChild(eventoDiv);
     });
+
 }
 
 function mostrarConciertos(){
@@ -72,35 +72,8 @@ function mostrarDeportes(){
     mostrarEventos(deportes);
 }
 
-function mostrarDomingueando(){
+function mostrarDomingueandopage(){
     const domingueando = eventos.filter(evento => evento instanceof Domingueando);
     mostrarEventos(domingueando);
 }
 
-let eventos = []
-
-//Agrego conciertos
-eventos.push(new Concierto("3 personas para ir a ver a Ciro","Movistar Arena","08-08-2024","20:00","4","Ciro y los Persas","Rock","https://chat.whatsapp.com/Gh2UhTbBNig0BrjrUJTCnj"))
-eventos.push(new Concierto("Ver a Maria Becerra con 3 chicas","Monumental - River","10-10-2024","19:00","4","María Becerra","Reguetón","https://chat.whatsapp.com/Gh2UhTbBNig0BrjrUJTCnj"))
-eventos.push(new Concierto("Ver a Babasónicos","Plaza de la música- Córdoba","08-11-2024","20:30","3","Babasónicos","Pop Rock","https://chat.whatsapp.com/Gh2UhTbBNig0BrjrUJTCnj"))
-eventos.push(new Concierto("Ver a Las Pastillas del Abuelo","Villa Ballester","13-07-2024","18:00","4","Pastillas del Abuelo","Rock","https://chat.whatsapp.com/Gh2UhTbBNig0BrjrUJTCnj"))
-eventos.push(new Concierto("Ver a Eric Clapton","Estadio José Amalfitani- cancha Vélez","20-09-2024","18:00","3","Eric Clapton","Rock","https://chat.whatsapp.com/Gh2UhTbBNig0BrjrUJTCnj"))
-eventos.push(new Concierto("Ver Iron Maiden","Estadio Tomás Adolfo Ducó- cancha Huracán","01-12-2024","19:00","4","Iron Maiden","Metal","https://chat.whatsapp.com/Gh2UhTbBNig0BrjrUJTCnj"))
-
-//Agrego deportes
-eventos.push(new Deporte("3 personas para el padel","Complejo deportivo Gonnet","14-07-2024","20:00","4","Deportivo","https://chat.whatsapp.com/Gh2UhTbBNig0BrjrUJTCnj"))
-eventos.push(new Deporte("9 para jugar al futbol","Cancha Predio Norte","21-07-2024","21:00","10","Deportivo","https://chat.whatsapp.com/Gh2UhTbBNig0BrjrUJTCnj"))
-eventos.push(new Deporte("3 para tenis","Predio Agua Potable","13-07-2024","10:00","4","Deportivo","https://chat.whatsapp.com/Gh2UhTbBNig0BrjrUJTCnj"))
-eventos.push(new Deporte("4 para turno de hockey","Predio club universitario","16-07-2024","16:00","5","Deportivo","https://chat.whatsapp.com/Gh2UhTbBNig0BrjrUJTCnj"))
-eventos.push(new Deporte("1 para ajedrez","Plaza San Martín","11-07-2024","12:00","2","Deportivo","https://chat.whatsapp.com/Gh2UhTbBNig0BrjrUJTCnj"))
-eventos.push(new Deporte("3 para correr","El Rosedal de Palermo","15-07-2024","19:00","4","Deportivo","https://chat.whatsapp.com/Gh2UhTbBNig0BrjrUJTCnj"))
-
-
-
-//Agrego domingueando
-eventos.push(new Domingueando("3 personas para tomar mates","Plaza Moreno","14-07-2024","15:00","4","Recreativo","https://chat.whatsapp.com/Gh2UhTbBNig0BrjrUJTCnj"))
-eventos.push(new Domingueando("5 para ir a cenar a un tenedor libre","Parrilla El Retiro","14-07-2024","21:00","5","Recreativo","https://chat.whatsapp.com/Gh2UhTbBNig0BrjrUJTCnj"))
-eventos.push(new Domingueando("2 para ir a pescar","Laguna la Salada","21-07-2024","06:00","3","Recreativo","https://chat.whatsapp.com/Gh2UhTbBNig0BrjrUJTCnj"))
-eventos.push(new Domingueando("1 para ir al cine a ver Intensamente 2","Cinema Rocha","14-07-2024","18:00","2","Recreativo","https://chat.whatsapp.com/Gh2UhTbBNig0BrjrUJTCnj"))
-eventos.push(new Domingueando("3 para hacer club de lectura","Café La Callecita","28-07-2024","16:00","4","Recreativo","https://chat.whatsapp.com/Gh2UhTbBNig0BrjrUJTCnj"))
-eventos.push(new Domingueando("4 para ver final de Copa América","Antares Cervecería","14-07-2024","20:30","5","Recreativo","https://chat.whatsapp.com/Gh2UhTbBNig0BrjrUJTCnj"))
