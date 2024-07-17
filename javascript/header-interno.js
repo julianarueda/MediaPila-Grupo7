10fc2155449a327498df712e331aead9124c0f2a
Juli <julianarueda40@gmail.com>
2024-07-17 01:12:44 -0300

    se agrega header para uso interno, como si estuviera logueado

---
+++ javascript/header-interno.js
@@ -0,0 +1,23 @@
+class Header extends HTMLElement {
+    constructor() {
+      super();
+    }
+
+    connectedCallback() {
+      this.innerHTML = `
+        <nav class="navbar navbar-expand-lg" id="barraDeNavegación">
+            <div class="logo">
+                <a href="/">
+                  <img class="circular-img" src="../img/Logo.png" alt="Logo">
+                </a>
+            </div>
+            <div id="items-navbar">
+            <a href="./perfil.html">MI PERFIL</a>
+            <a href="./misgrupos.html">MIS GRUPOS</a>
+            </div>
+        </nav>
+
+      `;
+    }
+  }
+  customElements.define('header-component', Header);
